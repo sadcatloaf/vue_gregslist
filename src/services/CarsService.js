0353beeac71a44247f92747ee33e7eb79c8f23ee
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-04 09:55:16 -0700

    storing cars in appstate

--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Car } from "@/models/Car.js"
+import { AppState } from "@/AppState.js"
 
 class CarsService {
   async getCars() {
     const response = await api.get('api/cars')
     logger.log('GOT CARS 🚗', response.data)
+    const cars = response.data.map(carPOJO => new Car(carPOJO))
+    AppState.cars = cars
   }
 }
 
